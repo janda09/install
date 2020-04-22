b08132c6d44f6d2f38cdc3efc28442b3dfc51145
janda09 <64169688+janda09@users.noreply.github.com>
2020-04-23 06:04:16 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e ""
+echo -e "For Debian 9 32 bit & 64 bit" | lolcat
+echo -e "For VPS with KVM and VMWare virtualization" | lolcat
+echo -e ""
+echo -e "Original script by :" | lolcat
+echo -e "* Fornesia" | lolcat
+echo -e "* Rzengineer" | lolcat
+echo -e "* Fawzya" | lolcat
+echo -e ""
+echo -e "Mod by Janda Baper Group" | lolcat
+echo -e ""
+echo -e "jandabaper09@gmail.com" | lolcat
+echo -e ""
